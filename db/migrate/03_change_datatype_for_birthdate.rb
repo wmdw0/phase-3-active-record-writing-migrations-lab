86e51d750522380e0737be36c11d2ad408caac29
William Walsh <wmdwalsh0@gmail.com>
2021-10-30 16:18:25 -0400

    commit tree e5becf0a2d18833b1b42f04c2b15906fc639479b

---
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -0,0 +1,7 @@
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[6.1]
+    def change
+      change_column :students, :grade, :integer
+      change_column :students, :birthdate, :datetime
+      
+    end
+  end
